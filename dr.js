4a3f3c84bdfcc485dc90cb255da9af74017e13b8
Niladrim14 <niladrim004@gmail.com>
2025-09-05 17:13:17 +0530

    EJS

---
+++ dr.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+   app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+app.listen(3000, () => {
+     console.log("Server is running on port 3000");
+});
