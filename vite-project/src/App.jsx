c0cea237f6808781c270b7a3a964615131d7b529
Niladrim14 <niladrim004@gmail.com>
2025-09-27 00:47:31 +0530

    taiwindcss add

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,25 +1,13 @@
+import React from 'react';
+
 import { useState } from "react";
 
  function App() {
-   
-  let [counter,setCounter]= useState(15)
-  const addvalue = ()=>{
-    if(counter<20)
-    setCounter(counter+1)
-  else {
-    h3
-  }
-  } 
-  const lessvalue = ()=>{
-    if(counter>0)
-    setCounter(counter-1)}
+ 
+  
   return (
     <>
-    <h1>Value : {counter}</h1>
-    <button onClick={addvalue}>Increase </button>
-    <button onClick={lessvalue}>Decrease </button>
-    
-    
+    <h1>Value </h1>
     </>
   );
 }
