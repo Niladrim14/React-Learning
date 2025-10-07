107e25338efb01e8d8421003728bfeb3813fa6fe
Niladrim14 <niladrim004@gmail.com>
2025-10-08 02:34:11 +0530

    testing after a long time

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,11 +4,21 @@ import { useState } from "react";
 
  function App() {
  
+  const [color,setcolor] = useState("Yellow")
   
   return (
-    <>
-    <h1>Value </h1>
-    </>
+    <div style={{backgroundColor : color }}>
+    <div className='h-screen border-white px-104 py-140 m-4'>
+    <button onClick={()=> setcolor("blue")}
+    className='px-4 py-6 text-2xl text-white' style={{backgroundColor : "blue"}}>Blue</button>
+
+ <button onClick={()=> setcolor("green")}
+    className='px-4 py-6 text-2xl text-white' style={{backgroundColor : "green"}}>green</button>
+
+     <button onClick={()=> setcolor("red")}
+    className='px-4 py-6 text-2xl text-white' style={{backgroundColor : "red"}}>red</button>
+    </div>
+    </div>
   );
 }
 export default App
